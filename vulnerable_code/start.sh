db05bd1f670e94882ac1c59795f99a375406621b
bravosixgoingdark <minhnhatbg05@gmail.com>
2024-12-01 14:05:30 +0700

    Add the ability to disable ASLR

--- vulnerable_code/start.sh
+++ vulnerable_code/start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+echo 0 | sudo tee /proc/sys/kernel/randomize_va_space # disable ASLR, dangerous, please set it back to 2 after finishing
 docker build -t vuln_program .
 docker run -d -p 1337:1337 --security-opt seccomp=unconfined vuln_program:latest
